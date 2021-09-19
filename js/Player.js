340bbcd3fc42396724947bfe9d1ed621bd433160
khushi200613 <85346152+khushi200613@users.noreply.github.com>
2021-09-19 18:15:38 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,60 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfo = database.ref("players")
+    playerInfo.on('value',function(data){
+      allPlayers = data.val()
+    })
+  }
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY
+    });
+  }
+  getDistance(){
+    var distance="players/player"+this.index;
+    database.ref(distance);
+    distance.on('value',function(data){
+      var dof=data.val()
+      this.positionX=dof.positionX;
+      this.positionY=dof.positionY;
+    })
+  }
+}
